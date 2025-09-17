41fd70efa36854de9e7b4249e851b2087c93ee5d
Cedrickkouek <66407596+Cedrickkouek@users.noreply.github.com>
2025-09-17 13:55:45 -0400

    "fix: grid size static number fixed"

--- src/Grid.java
+++ src/Grid.java
@@ -3,11 +3,15 @@ import java.awt.*;
 public class Grid {
     private final  char[][] grid;
 
-    public Grid(){
-        grid = new char[4][4];
+    public Grid(int seize){
+        grid = new char[seize][seize];
     }
 
     public char readCell(Point cell){
         return grid[cell.x][cell.y];
+    }
+
+    public int getSize() {
+        return grid.length;
     }
 }
--- tests/GridTest.java
+++ tests/GridTest.java
@@ -6,15 +6,22 @@ import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GridTest {
+    private static final  int GRID_SIZE = 4;
+
     private Grid grid;
 
     @BeforeEach
     public void setUp() {
-        grid = new Grid();
+        grid = new Grid(GRID_SIZE);
     }
 
     @Test
     public void readCellInEmptyGridTest() {
         assertEquals(Character.MIN_VALUE, grid.readCell(new Point(0, 0)));
+    }
+
+    @Test
+    public void testGetSizeShouldReturnGridSize() {
+        assertEquals(4, grid.getSize());
     }
 }
