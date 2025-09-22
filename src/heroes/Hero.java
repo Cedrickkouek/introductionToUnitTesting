a791285f8b2d162db4502e86caa51440f7418ab4
Cedrickkouek <66407596+Cedrickkouek@users.noreply.github.com>
2025-09-22 10:07:51 -0400

    "feat: add abilities to hero"

--- src/heroes/Hero.java
+++ src/heroes/Hero.java
@@ -1,13 +1,33 @@
 package heroes;
 
+import heroes.abilities.Ability;
+
 import java.util.ArrayList;
 
 public class Hero {
     private final String name;
     private final char symbol;
+    private ArrayList<Ability> abilities;
 
-    public Hero(String name, char symbol) {
+    public Hero(String name, char symbol, ArrayList<Ability> abilities) {
         this.name = name;
         this.symbol = symbol;
+        this.abilities = abilities;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public ArrayList<Ability> getAbilities() {
+        return abilities;
+    }
+
+    public Ability getAbility(int index){
+        return abilities.get(index);
     }
 }
